a83167a79337a94dc03f39a7d74a882e2578a44d
Yao-Hui Chua <hello@yao.page>
2026-02-08 05:03:48 -0800

    Set up React and Vite boilerplate in montauk-ui

---
+++ packages/montauk-ui/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black h-screen w-screen flex items-center justify-center font-mono">
+      <h1 className="text-4xl text-red-500">CABAL ONLINE</h1>
+    </div>
+  );
+}
+
+export default App;
